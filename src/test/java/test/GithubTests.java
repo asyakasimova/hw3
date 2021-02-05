0a6961bd6b7ae82bd8615c9d6788311cb0ce6991
Valkyrja <kilyanet@gmail.com>
2021-02-05 23:03:23 +0300

    New project for GitHub Tests

---
+++ src/test/java/test/GithubTests.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class GithubTests {
+
+    @Test
+    void checkSelenideGithubTest() {
+        // Откройте страниц Selenide в Github
+
+        // Перейдите в раздел Wiki проекта
+
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+    }
+
+}
